e379fda02d896da95daf047fb5ae131f3b2831c6
Janis Goesser <jgoesser@ailio.de>
2021-02-12 01:48:48 +0100

    Add Djangocorsheaders and include media to urls

--- djangoblog/config/settings.py
+++ djangoblog/config/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +39,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
 
--- djangoblog/config/urls.py
+++ djangoblog/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from blog import views as blog_views
 
@@ -18,4 +20,4 @@ urlpatterns = [
         blog_views.BlogDetailAPI.as_view(),
         name="BlogBlogDetailAPI"
     )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
