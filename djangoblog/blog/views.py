da3805665008e1488795ed437b5d907085576e48
Janis Goesser <jgoesser@ailio.de>
2021-02-13 17:10:46 +0100

    Update Timeout on cached views

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -11,9 +11,11 @@ class BlogPreviewAPI(CachedGenericView, ListAPIView):
         .order_by("-published_date")
     serializer_class = BlogPreviewSerializer
     pagination_class = BlogPreviewPagination
+    timeout = 60 * 24
 
 
 class BlogDetailAPI(CachedGenericView, RetrieveAPIView):
     queryset = Blogpost.objects
     serializer_class = BlogDetailSerializer
     lookup_field = 'title_slug'
+    timeout = 60 * 24
